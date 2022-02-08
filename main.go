901d1534f2553c068b514354f3c15fa2ca1bd761
Ayush Suman <ayush231suman@gmail.com>
2022-02-08 21:45:14 +0530

    Attempt to Implement GSign In

--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func defaultHander(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", signIn)
+
+	http.HandleFunc("/", defaultHander)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func signIn(w http.ResponseWriter, r *http.Request) {
+	key := r.Header.Get("Auth-Key")
+	fmt.Fprintf(w, "%s", key)
 }
