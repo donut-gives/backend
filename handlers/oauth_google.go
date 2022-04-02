4d7fa5ea743193cc3e7dc92dd424ed5eed79dba9
Ayush Suman <ayush231suman@gmail.com>
2022-04-02 19:18:23 +0530

    Google Sign In: Attempt 2 Add oAuthState

--- handlers/oauth_google.go
+++ handlers/oauth_google.go
@@ -1,17 +1,15 @@
 package handlers
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 var googleOauthConfig = &oauth2.Config{}
@@ -19,37 +17,43 @@ var googleOauthConfig = &oauth2.Config{}
 const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 
 func oauthGoogleLogin(w http.ResponseWriter, r *http.Request) {
-    googleOauthConfig = &oauth2.Config{
+	googleOauthConfig = &oauth2.Config{
 		RedirectURL:  "https://" + r.Host + "/auth/google/callback",
 		ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
 		Scopes:       []string{"openid", "profile", "email"},
 		Endpoint:     google.Endpoint,
 	}
-	u := googleOauthConfig.AuthCodeURL(oauthState)
+	u := googleOauthConfig.AuthCodeURL("donut")
 	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
 }
 
-
 func oauthGoogleCallback(w http.ResponseWriter, r *http.Request) {
-    if googleOauthConfig.RedirectURL == nil {
-        googleOauthConfig = &oauth2.Config{
-        		RedirectURL:  "https://" + r.Host + "/auth/google/callback",
-        		ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
-        		ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
-        		Scopes:       []string{"openid", "profile", "email"},
-        		Endpoint:     google.Endpoint,
-        	}
-    }
+	if r.FormValue("state") != "donut" {
+		log.Println("invalid oauth google state")
+		http.Redirect(w, r, "/auth/google/login", http.StatusTemporaryRedirect)
+		return
+	}
+	if googleOauthConfig.RedirectURL == "" {
+		googleOauthConfig = &oauth2.Config{
+			RedirectURL:  "https://" + r.Host + "/auth/google/callback",
+			ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
+			ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
+			Scopes:       []string{"openid", "profile", "email"},
+			Endpoint:     google.Endpoint,
+		}
+	}
 	data, err := getUserDataFromGoogle(r.FormValue("code"))
 	if err != nil {
 		log.Println(err.Error())
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
-	fmt.Fprintf(w, "UserInfo: %s\n", data)
+	_, err = fmt.Fprintf(w, "UserInfo: %s\n", data)
+	if err != nil {
+		return
+	}
 }
-
 
 func getUserDataFromGoogle(code string) ([]byte, error) {
 	token, err := googleOauthConfig.Exchange(context.Background(), code)
@@ -60,7 +64,12 @@ func getUserDataFromGoogle(code string) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
 	}
-	defer response.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(response.Body)
 	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed read response: %s", err.Error())
