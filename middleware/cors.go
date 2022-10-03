d4817d85125d273c7029433cef7107d23c4f29ea
Ayush Suman <ayush231suman@gmail.com>
2022-10-03 21:00:04 +0530

    Contact Us

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,7 +2,7 @@ package middleware
 
 import "github.com/gin-gonic/gin"
 
-func CORSMiddleware() gin.HandlerFunc {
+func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
